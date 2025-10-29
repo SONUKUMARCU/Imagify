4b879dbdd98747c842ada83cfc91aebce8b06451
SONUKUMARCU <sonukumar.cu48@gmail.com>
2025-10-29 23:52:35 +0530

    models created and mongodb connected

--- server/server.js
+++ server/server.js
@@ -1,15 +1,15 @@
 import express from "express"
 import cors from "cors"
 import 'dotenv/config'
+import connectDb from "./config/mongodb.js"
 
 
 const PORT = process.env.PORT || 3000
-
 const app = express();
 
 app.use(express.json())
 app.use(cors())
-
+await connectDb();
 
 app.get("/",(req,res) => res.send("API is working"))
 
